6d9872fc8704429adf39752f251f700c9ac655ae
ak-matsu <sandbox.matsu@gmail.com>
2021-03-14 17:06:37 +0900

    問題2 新規投稿するとエラーが生じる

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets, only: [:new, :index, :edit, :show, :update, :destroy] do  
+  # createを追加するが、resourcesは7つのアクションをまとめたものであり、
+  # 全て揃うと個別の指定はしなくてよいため、resources :tweets doとなる
+  resources :tweets do
     resources :comments, only: :create
     collection do
       get 'search'
